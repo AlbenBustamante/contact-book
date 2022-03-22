d7db7f6062f459d9aa87cc62b1ffd2fc6ed23245
alnicode32 <albensnico@gmail.com>
2022-03-21 19:29:56 -0500

    Creada la entidad para Phone

---
+++ src/main/java/com/alnicode/contactbook/persistence/entity/PhoneEntity.java
@@ -0,0 +1,27 @@
+package com.alnicode.contactbook.persistence.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "phones")
+public class PhoneEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "phone_id")
+    private Long phoneId;
+
+    @Column(nullable = false, unique = true)
+    private String number;
+
+    private String type;
+}
