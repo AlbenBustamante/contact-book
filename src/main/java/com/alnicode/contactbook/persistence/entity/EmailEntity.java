7990fc4c06d85218a1966d2932f1d2084c06b0fa
alnicode32 <albensnico@gmail.com>
2022-03-21 19:34:06 -0500

    Creada la entidad para los emails

---
+++ src/main/java/com/alnicode/contactbook/persistence/entity/EmailEntity.java
@@ -0,0 +1,27 @@
+package com.alnicode.contactbook.persistence.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "emails")
+public class EmailEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "email_id")
+    private Long emailId;
+
+    @Column(nullable = false, unique = true)
+    private String direction;
+
+    private String type;
+}
