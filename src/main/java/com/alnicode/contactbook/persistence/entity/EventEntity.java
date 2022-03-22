17359862a1671ac8e2f3e504a45df60cdf0c4f4b
alnicode32 <albensnico@gmail.com>
2022-03-21 19:42:16 -0500

    Creada la entidad para los eventos

---
+++ src/main/java/com/alnicode/contactbook/persistence/entity/EventEntity.java
@@ -0,0 +1,28 @@
+package com.alnicode.contactbook.persistence.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "events")
+public class EventEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "event_id")
+    private Long eventId;
+
+    @Column(nullable = false)
+    private LocalDate date;
+
+    private String type;
+}
