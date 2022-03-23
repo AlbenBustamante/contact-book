3098546a13809998853f0ac02ec561a2a4a197c4
alnicode32 <albensnico@gmail.com>
2022-03-23 02:24:28 -0500

    Creado método para mapear una lista de entidades a dto

--- src/main/java/com/alnicode/contactbook/persistence/mapper/ContactMapper.java
+++ src/main/java/com/alnicode/contactbook/persistence/mapper/ContactMapper.java
@@ -4,11 +4,14 @@ import com.alnicode.contactbook.domain.dto.Contact;
 import com.alnicode.contactbook.persistence.entity.ContactEntity;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring", uses = {
         DirectionMapper.class, EmailMapper.class, EventMapper.class,
         PhoneMapper.class, ContactsWebsiteMapper.class})
 public interface ContactMapper {
     Contact toContact(ContactEntity contactEntity);
+    List<Contact> toContacts(List<ContactEntity> contactsEntity);
 
     ContactEntity toContactEntity(Contact contact);
 }
