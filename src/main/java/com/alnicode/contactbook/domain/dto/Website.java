49a745d3712333a54fb4612f67ac89c4faa30518
alnicode32 <albensnico@gmail.com>
2022-03-22 20:13:49 -0500

    Creado modelo dto para Website

---
+++ src/main/java/com/alnicode/contactbook/domain/dto/Website.java
@@ -0,0 +1,11 @@
+package com.alnicode.contactbook.domain.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Website {
+    private long websiteId;
+    private String direction;
+}
