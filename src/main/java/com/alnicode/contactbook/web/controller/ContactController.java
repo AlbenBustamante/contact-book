86676ecae7fa0b1d2fd3c9573745b53295f91f28
alnicode32 <albensnico@gmail.com>
2022-03-24 01:42:23 -0500

    Creado método para registrar a un nuevo contacto

--- src/main/java/com/alnicode/contactbook/web/controller/ContactController.java
+++ src/main/java/com/alnicode/contactbook/web/controller/ContactController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,5 +34,10 @@ public class ContactController {
     @GetMapping("/{name}")
     public ResponseEntity<Contact> getByName(@PathVariable("name") String name) {
         return ResponseEntity.of(this.service.getByName(name));
+    }
+
+    @PostMapping
+    public ResponseEntity<Contact> save(@RequestBody Contact contact) {
+        return new ResponseEntity<>(this.service.save(contact), HttpStatus.CREATED);
     }
 }
