0d5eec51015bc94d450d613f96e8c2b93087d20b
alnicode32 <albensnico@gmail.com>
2022-03-24 01:24:38 -0500

    Implementados los métodos del servicio a excepción del método delete

--- src/main/java/com/alnicode/contactbook/domain/service/impl/ContactServiceImpl.java
+++ src/main/java/com/alnicode/contactbook/domain/service/impl/ContactServiceImpl.java
@@ -2,6 +2,7 @@ package com.alnicode.contactbook.domain.service.impl;
 
 import com.alnicode.contactbook.domain.dto.Contact;
 import com.alnicode.contactbook.domain.service.ContactService;
+import com.alnicode.contactbook.persistence.entity.ContactEntity;
 import com.alnicode.contactbook.persistence.mapper.ContactMapper;
 import com.alnicode.contactbook.persistence.repository.ContactRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,8 @@ import java.util.Optional;
 @Service
 public class ContactServiceImpl implements ContactService {
 
+    private static final String CONTACT_NOT_FOUND_MESSAGE = "Contact not found!";
+
     @Autowired
     private ContactRepository repository;
 
@@ -22,27 +25,41 @@ public class ContactServiceImpl implements ContactService {
 
     @Override
     public List<Contact> getAll() {
-        return null;
+        List<ContactEntity> contacts = (List<ContactEntity>) this.repository.findAll();
+        return this.mapper.toContacts(contacts);
     }
 
     @Override
     public Optional<Contact> getContact(long contactId) {
-        return Optional.empty();
+        return this.repository.findById(contactId).map(contact -> this.mapper.toContact(contact));
     }
 
     @Override
     public Optional<Contact> getByName(String name) {
-        return Optional.empty();
+        return this.repository.findByName(name).map(contact -> this.mapper.toContact(contact));
     }
 
     @Override
     public Contact save(Contact contact) {
-        return null;
+        ContactEntity contactEntity = this.mapper.toContactEntity(contact);
+        contactEntity.getWebsites().forEach(website -> website.setContact(contactEntity));
+        return this.mapper.toContact(contactEntity);
     }
 
     @Override
     public Contact update(long contactId, Contact contact) {
-        return null;
+        return this.getContact(contactId).map(newContact -> {
+            newContact.setContactId(contactId);
+            newContact.setDirections(contact.getDirections());
+            newContact.setEmails(contact.getEmails());
+            newContact.setPhones(contact.getPhones());
+            newContact.setEvents(contact.getEvents());
+            newContact.setWebsites(contact.getWebsites());
+            newContact.setName(contact.getName());
+
+            ContactEntity newEntity = this.mapper.toContactEntity(newContact);
+            return this.mapper.toContact(this.repository.save(newEntity));
+        }).orElseThrow(() -> new RuntimeException(CONTACT_NOT_FOUND_MESSAGE));
     }
 
     @Override
