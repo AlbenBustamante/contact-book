36e3a69fc1e7a27cedd0c543925d867b53f4c99b
alnicode32 <albensnico@gmail.com>
2022-03-24 01:38:53 -0500

    Creado método para obtener a un contacto según su id

--- src/main/java/com/alnicode/contactbook/web/controller/ContactController.java
+++ src/main/java/com/alnicode/contactbook/web/controller/ContactController.java
@@ -5,6 +5,7 @@ import com.alnicode.contactbook.domain.service.ContactService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +20,9 @@ public class ContactController {
 
     public ResponseEntity<List<Contact>> getAll() {
         return new ResponseEntity<>(this.service.getAll(), HttpStatus.OK);
+    }
+
+    public ResponseEntity<Contact> getContact(@PathVariable("id") long contactId) {
+        return ResponseEntity.of(this.service.getContact(contactId));
     }
 }
